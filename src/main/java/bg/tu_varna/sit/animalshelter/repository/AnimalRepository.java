e18d54be061187edd0e200b86f0ad1a32f7b1158
ViktorSmkv00 <v.semkov47@gmail.com>
2024-09-22 22:32:35 +0300

    Updated AnimalRepository

--- src/main/java/bg/tu_varna/sit/animalshelter/repository/AnimalRepository.java
+++ src/main/java/bg/tu_varna/sit/animalshelter/repository/AnimalRepository.java
@@ -4,9 +4,7 @@ package bg.tu_varna.sit.animalshelter.repository;
 import bg.tu_varna.sit.animalshelter.model.Animal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
-import java.util.UUID;
 
 @Repository
 public interface AnimalRepository extends JpaRepository<Animal, Long> {
